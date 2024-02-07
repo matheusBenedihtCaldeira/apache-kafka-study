aa171fea7e0a514920da98be40513fb76ec56c33
matheusBenedihtCaldeira <matheusbenediht@gmail.com>
2024-02-07 10:49:50 -0300

    Update: custom listener

--- str-consumer/src/main/java/com/test/kafka/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/test/kafka/strconsumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package com.test.kafka.strconsumer.listeners;
 
+import com.test.kafka.strconsumer.custom.StrConsumerCustomListner;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -9,32 +10,17 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-            @TopicPartition(topic = "str-topic", partitions = {"0"})
-            }
-            , containerFactory = "strContainerFactory")
-
-    public void listener(String message){
-       log.info("Receive message {}", message);
+    @StrConsumerCustomListner(groupId = "group-1")
+    public void create(String message){
+       log.info("CREATE ::: Receive message {}", message);
     }
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-                    @TopicPartition(topic = "str-topic", partitions = {"1"})
-            }
-            , containerFactory = "strContainerFactory")
-
-    public void listener2(String message){
-        log.info("Receive message {}", message);
+    @StrConsumerCustomListner(groupId = "group-1")
+    public void log(String message){
+        log.info("LOG ::: Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-2",
-            topicPartitions = {
-                    @TopicPartition(topic = "str-topic", partitions = {"1"})
-            }
-            , containerFactory = "strContainerFactory")
-
-    public void listener3(String message){
-        log.info("Receive message {}", message);
+    @StrConsumerCustomListner(groupId = "group-2")
+    public void history(String message){
+        log.info("HISTORY  ::: Receive message {}", message);
     }
 }
